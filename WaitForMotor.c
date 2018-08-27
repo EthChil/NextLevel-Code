84df571e8cadb2e21308eeab7ea826ef0b4b673c
EthChil <ethanchilderhose@gmail.com>
2018-08-27 19:34:31 -0400

    Fixed formatting

--- WaitForMotor.c
+++ WaitForMotor.c
@@ -12,11 +12,11 @@ void waitForMotor(tMotor nMotorIndex) {
 
 task main()
 {
-setMotorTarget(LMotor,1000,50);
-setMotorTarget(RMotor,1000,50);
-waitForMotor(LMotor);
-setMotorTarget(LMotor,0,50);
-setMotorTarget(RMotor,0,50);
-waitForMotor(LMotor);
-stopAllMotors();
+	setMotorTarget(LMotor,1000,50);
+	setMotorTarget(RMotor,1000,50);
+	waitForMotor(LMotor);
+	setMotorTarget(LMotor,0,50);
+	setMotorTarget(RMotor,0,50);
+	waitForMotor(LMotor);
+	stopAllMotors();
 }
